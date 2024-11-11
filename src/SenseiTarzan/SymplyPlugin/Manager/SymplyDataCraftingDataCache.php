4bb9bcdb4ea3a2eaa3d1713fc4ea94e960f8d35b
SenseiTarzan <gabrielcaptari0@gmail.com>
2024-11-11 01:50:21 +0100

    add compatibility 5.21.0

--- src/SenseiTarzan/SymplyPlugin/Manager/SymplyDataCraftingDataCache.php
+++ src/SenseiTarzan/SymplyPlugin/Manager/SymplyDataCraftingDataCache.php
@@ -163,6 +163,8 @@ class SymplyDataCraftingDataCache
 				FurnaceType::FURNACE => FurnaceRecipeBlockName::FURNACE,
 				FurnaceType::BLAST_FURNACE => FurnaceRecipeBlockName::BLAST_FURNACE,
 				FurnaceType::SMOKER => FurnaceRecipeBlockName::SMOKER,
+				FurnaceType::CAMPFIRE => FurnaceRecipeBlockName::CAMPFIRE,
+				FurnaceType::SOUL_CAMPFIRE => FurnaceRecipeBlockName::SOUL_CAMPFIRE
 			};
 			foreach($manager->getFurnaceRecipeManager($furnaceType)->getAll() as $recipe){
 				$input = $converter->coreRecipeIngredientToNet($recipe->getInput())->getDescriptor();
