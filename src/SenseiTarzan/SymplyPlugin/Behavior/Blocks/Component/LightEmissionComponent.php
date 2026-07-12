02fcca8bec87c9d2464e5294673b97e6d3e38e5e
Back <100467970+YnXahgkiO@users.noreply.github.com>
2026-07-12 21:46:16 +0200

    Added LightEmissionComponent

---
+++ src/SenseiTarzan/SymplyPlugin/Behavior/Blocks/Component/LightEmissionComponent.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ *
+ *            _____ _____         _      ______          _____  _   _ _____ _   _  _____
+ *      /\   |_   _|  __ \       | |    |  ____|   /\   |  __ \| \ | |_   _| \ | |/ ____|
+ *     /  \    | | | |  | |______| |    | |__     /  \  | |__) |  \| | | | |  \| | |  __
+ *    / /\ \   | | | |  | |______| |    |  __|   / /\ \ |  _  /| . ` | | | | . ` | | |_ |
+ *   / ____ \ _| |_| |__| |      | |____| |____ / ____ \| | \ \| |\  |_| |_| |\  | |__| |
+ *  /_/    \_\_____|_____/       |______|______/_/    \_\_|  \_\_| \_|_____|_| \_|\_____|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author AID-LEARNING
+ * @link https://github.com/AID-LEARNING
+ *
+ */
+
+declare(strict_types=1);
+
+namespace SenseiTarzan\SymplyPlugin\Behavior\Blocks\Component;
+
+use BackedEnum;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\Tag;
+use SenseiTarzan\SymplyPlugin\Behavior\Common\Component\AbstractComponent;
+
+final class LightEmissionComponent extends AbstractComponent
+{
+	public function __construct(private readonly int $emission)
+	{
+	}
+
+	public function getName() : string|BackedEnum
+	{
+		return "minecraft:light_emission";
+	}
+
+	protected function value() : Tag
+	{
+		return CompoundTag::create()->setByte("emission", $this->emission);
+	}
+}
